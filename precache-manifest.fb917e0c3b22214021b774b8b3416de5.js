35c5b73c2e8ceb89e44a6900724ed5ad64e905b7
Rahul7star <rahul7star@gmail.com>
2018-10-15 08:52:14 +1100

    Updates

---
+++ precache-manifest.fb917e0c3b22214021b774b8b3416de5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ReactWeb/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "328d1bf238c543be3c62",
+    "url": "/ReactWeb/static/js/runtime~main.328d1bf2.js"
+  },
+  {
+    "revision": "fc60c5839831707cdbce",
+    "url": "/ReactWeb/static/js/main.fc60c583.chunk.js"
+  },
+  {
+    "revision": "6105a37ce77693a684b2",
+    "url": "/ReactWeb/static/js/1.6105a37c.chunk.js"
+  },
+  {
+    "revision": "fc60c5839831707cdbce",
+    "url": "/ReactWeb/static/css/main.e73a2400.chunk.css"
+  },
+  {
+    "revision": "b57c93aa609beaa1933002f7ee785848",
+    "url": "/ReactWeb/index.html"
+  }
+];
